74e083cd6d7c0e0c345b19538b65d751a2de1159
Kurkumich <113376198+Kurkumich@users.noreply.github.com>
2023-05-02 15:26:03 +0300

    Update forma.php

--- forma.php
+++ forma.php
@@ -2,9 +2,9 @@
  
 // Настройки подключения к базе данных
 $servername = "localhost";
-$username = "u52988";
-$password = "4622873";
-$dbname = "u52988";
+$username = "u52987";
+$password = "9793494";
+$dbname = "u52987";
  
 // Создание подключения
 try {
